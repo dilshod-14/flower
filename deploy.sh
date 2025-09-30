14c3942407fcffefbd3f50cb85810a9b7e19fcd6
dilshod-14 <urolovdilshod24@gmail.com>
2025-09-30 15:57:23 +0900

    feat: deploy project via shell & pm2

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # PRODUCTION
-git reset --hard
-git checkout master
-git pull origin master
+# git reset --hard
+# git checkout master
+# git pull origin master
 
 npm i yarn -g
 yarn global add serve
 yarn 
 yarn run build
-pm2 start "yarn run start:prod" --name=BURAK-REACT
+pm2 start "yarn run start:prod" --name=FLOWER-REACT
